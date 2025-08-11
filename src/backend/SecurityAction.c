5105a196166393815fa9f9480e5be62ad564d291
Joshua Stone <joshua.stone@jhuapl.edu>
2025-08-11 13:48:49 -0400

    new files

---
+++ src/backend/SecurityAction.c
@@ -0,0 +1,79 @@
+#include "SecurityAction.h"
+
+size_t BSL_SecurityAction_Sizeof(void)
+{
+    return sizeof(BSL_SecurityAction_t);
+}
+
+bool BSL_SecurityAction_IsConsistent(const BSL_SecurityAction_t *self)
+{
+    (void) self;
+    return true;
+}
+
+void BSL_SecurityAction_Init(BSL_SecurityAction_t *self)
+{
+    ASSERT_ARG_NONNULL(self);
+
+    BSL_SecOperList_init(self->sec_op_list);
+    self->sec_op_list_length = 0;
+    self->err_ct = 0;
+}
+
+void BSL_SecurityAction_Deinit(BSL_SecurityAction_t *self)
+{
+    ASSERT_ARG_NONNULL(self);
+
+    BSL_SecOperList_clear(self->sec_op_list);
+}
+
+void BSL_SecurityAction_IncrError(BSL_SecurityAction_t *self)
+{
+    ASSERT_ARG_NONNULL(self);
+    self->err_ct++;
+}
+
+size_t BSL_SecurityAction_CountErrors(const BSL_SecurityAction_t *self)
+{
+    ASSERT_ARG_NONNULL(self);
+    return self->err_ct;
+}
+
+int BSL_SecurityAction_AppendSecOper(BSL_SecurityAction_t *self, BSL_SecOper_t *sec_oper)
+{
+    ASSERT_ARG_NONNULL(self);
+
+    BSL_SecOperList_it_t it;
+    for (BSL_SecOperList_it(it, self->sec_op_list); !BSL_SecOperList_end_p(it); BSL_SecOperList_next(it))
+    {
+        if (BSL_SecOper_GetTargetBlockNum(BSL_SecOperList_cref(it)) == BSL_SecOper_GetTargetBlockNum(sec_oper))
+        {
+            if (!(BSL_SecOper_IsBIB(BSL_SecOperList_cref(it)) ^ BSL_SecOper_IsBIB(sec_oper)))
+            {
+                BSL_SecOper_SetConclusion(sec_oper, BSL_SECOP_CONCLUSION_INVALID);
+            }
+            BSL_SecOperList_insert(self->sec_op_list, it, *sec_oper);
+            self->sec_op_list_length ++;
+            return BSL_SUCCESS;
+        }
+    }
+
+    // Target not shared, order doesn't matter
+
+    BSL_SecOperList_push_back(self->sec_op_list, *sec_oper);
+    self->sec_op_list_length ++;
+
+    return BSL_SUCCESS;
+}
+
+size_t BSL_SecurityAction_CountSecOpers(const BSL_SecurityAction_t *self)
+{
+    ASSERT_ARG_NONNULL(self);
+    return self->sec_op_list_length;
+}
+
+const BSL_SecOper_t *BSL_SecurityAction_GetSecOperAtIndex(const BSL_SecurityAction_t *self, size_t index)
+{
+    ASSERT_ARG_NONNULL(self);
+    return BSL_SecOperList_cget(self->sec_op_list, index);
+}
---
+++ src/backend/SecurityAction.h
@@ -0,0 +1,14 @@
+#include "m-list.h"
+#include <BPSecLib_Private.h>
+#include "SecOperation.h"
+
+// NOLINTBEGIN
+LIST_DEF(BSL_SecOperList, BSL_SecOper_t, M_OPEXTEND(M_POD_OPLIST, CLEAR(API_2(BSL_SecOper_Deinit))))
+// NOLINTEND
+
+struct BSL_SecurityAction_s
+{
+    BSL_SecOperList_t sec_op_list;
+    size_t sec_op_list_length;
+    size_t err_ct;
+};
