2f489d359ded160db52134e8c3e584a0c389a8d6
Joshua Stone <joshua.stone@jhuapl.edu>
2025-08-12 11:48:31 -0400

    invalid conclusion

--- src/backend/SecurityAction.c
+++ src/backend/SecurityAction.c
@@ -71,6 +71,13 @@ int BSL_SecurityAction_AppendSecOper(BSL_SecurityAction_t *self, BSL_SecOper_t *
         {
             // SOURCE BIB or ACCEPT BCB should come first
             // true if ACC BIB or SRC BCB
+
+            // Both BIBs or BCBs
+            if (!(BSL_SecOper_IsBIB(sec_oper) ^ BSL_SecOper_IsBIB(comp)))
+            {
+                BSL_SecOper_SetConclusion(sec_oper, BSL_SECOP_CONCLUSION_INVALID);
+            }
+
             if (BSL_SecOper_IsBIB(sec_oper) ^ BSL_SecOper_IsRoleSource(sec_oper))
             {
                 BSL_SecOperList_push_at(self->sec_op_list, i + 1, *sec_oper);
