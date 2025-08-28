NASA-AMMOS/BSL

commit 10ff73a825422870651585ed8a001a804caf1f95

Remove static-length `BSL_StaticString_t`  (#86)

src/policy_provider/SamplePolicyProvider.c
@@ -36,7 +36,6 @@
 static bool BSLP_PolicyProvider_IsConsistent(const BSLP_PolicyProvider_t *self)
 {
     ASSERT_ARG_NONNULL(self);
-    ASSERT_ARG_EXPR(strlen(self->name) < sizeof(self->name)); // TODO - Safer strlen since no strnlen
     ASSERT_ARG_EXPR(self->rule_count < (sizeof(self->rules) / sizeof(BSLP_PolicyRule_t)));
     return true;
 }
@@ -54,7 +53,6 @@ static bool BSLP_PolicyPredicate_IsConsistent(const BSLP_PolicyPredicate_t *self
 static bool BSLP_PolicyRule_IsConsistent(const BSLP_PolicyRule_t *self)
 {
     ASSERT_ARG_NONNULL(self);
-    ASSERT_ARG_EXPR(strlen(self->description) < sizeof(self->description));
     ASSERT_ARG_NONNULL(self->params);
     ASSERT_ARG_EXPR(BSL_SECROLE_ISVALID(self->role));
     ASSERT_ARG_EXPR(self->sec_block_type > 0);
@@ -355,7 +353,7 @@ int BSLP_PolicyRule_Init(BSLP_PolicyRule_t *self, const char *desc, BSLP_PolicyP
 {
     ASSERT_ARG_NONNULL(self);
     memset(self, 0, sizeof(*self));
-    strncpy(self->description, desc, sizeof(self->description) - 1);
+    string_init_set_str(self->description, desc);
     self->sec_block_type    = sec_block_type;
     self->target_block_type = target_block_type;
     self->predicate         = predicate;
@@ -373,6 +371,7 @@ void BSLP_PolicyRule_Deinit(BSLP_PolicyRule_t *self)
 {
     ASSERT_ARG_EXPR(BSLP_PolicyRule_IsConsistent(self));
     BSL_LOG_INFO("BSLP_PolicyRule_Deinit: %s, nparams=%zu", self->description, self->nparams);
+    string_clear(self->description);
     BSL_FREE(self->params);
     memset(self, 0, sizeof(*self));
 }

src/BPSecLib_Public.h
@@ -74,9 +74,6 @@ typedef struct BSL_SecCtxDesc_s BSL_SecCtxDesc_t;
 /// @brief Forward-declaration for structure containing callbacks to  provider.
 typedef struct BSL_PolicyDesc_s BSL_PolicyDesc_t;
 
-#define BSL_DEFAULT_STRLEN (128)
-typedef char BSL_StaticString_t[BSL_DEFAULT_STRLEN];
-
 /** @brief Indicates where in the lifecycle of the BPA the bundle is querying for security policy.
  *
  * @note The numeric values of the enum are arbitrary. We avoid using 0 as defaults.

src/backend/AbsSecBlock.c
@@ -54,7 +54,6 @@ bool BSL_AbsSecBlock_IsConsistent(const BSL_AbsSecBlock_t *self)
 
 void BSL_AbsSecBlock_Print(const BSL_AbsSecBlock_t *self)
 {
-    BSL_StaticString_t str;
     BSL_LOG_INFO("ASB  context id: %" PRId64, self->sec_context_id);
     for (size_t index = 0; index < uint64_list_size(self->targets); index++)
     {
@@ -70,9 +69,11 @@ void BSL_AbsSecBlock_Print(const BSL_AbsSecBlock_t *self)
     for (size_t index = 0; index < BSLB_SecResultList_size(self->results); index++)
     {
         BSL_SecResult_t *sec_result = BSLB_SecResultList_get(self->results, index);
-        BSL_Log_DumpAsHexString((uint8_t *)str, sizeof(str), sec_result->_bytes, sec_result->_bytelen);
+
+        char hex_str[2 * sec_result->_bytelen + 1];
+        BSL_Log_DumpAsHexString((uint8_t *)hex_str, sizeof(hex_str), sec_result->_bytes, sec_result->_bytelen);
         BSL_LOG_INFO("ASB  Result[%zu]: tgt=%" PRIu64 ", id=%" PRIu64 " %s", index, sec_result->target_block_num,
-                     sec_result->result_id, str);
+                     sec_result->result_id, hex_str);
     }
 }
 

src/policy_provider/SamplePolicyProvider.h
@@ -30,6 +30,7 @@
 
 #include <stdint.h>
 #include <m-array.h>
+#include <m-string.h>
 #include <BPSecLib_Private.h>
 
 // NOLINTBEGIN
@@ -98,7 +99,7 @@ bool BSLP_PolicyPredicate_IsMatch(const BSLP_PolicyPredicate_t *self, BSL_Policy
  */
 typedef struct BSLP_PolicyRule_s
 {
-    BSL_StaticString_t        description;
+    string_t                  description;
     BSLP_PolicyPredicate_t   *predicate;
     BSL_SecRole_e             role;
     BSL_BundleBlockTypeCode_e target_block_type;
@@ -158,7 +159,7 @@ int BSLP_PolicyRule_EvaluateAsSecOper(const BSLP_PolicyRule_t *self, BSL_SecOper
 /// @brief Concrete definition of a policy provider
 typedef struct BSLP_PolicyProvider_s
 {
-    BSL_StaticString_t     name;
+    string_t               name;
     BSLP_PolicyPredicate_t predicates[BSLP_POLICYPREDICATE_ARRAY_CAPACITY];
     size_t                 predicate_count;
     BSLP_PolicyRule_t      rules[BSLP_POLICYPREDICATE_ARRAY_CAPACITY];

test/bsl_test_utils.c
@@ -235,18 +235,9 @@ bool BSL_TestUtils_IsB16StrEqualTo(const char *b16_string, BSL_Data_t encoded_va
 
 void BSL_TestUtils_PrintHexToBuffer(const char *message, uint8_t *buff, size_t bufflen)
 {
-
-    uint8_t *ascii_buf = BSL_MALLOC(bufflen * 2 + 10);
-    memset(ascii_buf, 0, bufflen * 2 + 10);
-    const char hex_digits[] = "0123456789abcdef";
-    size_t     i;
-    for (i = 0; i < bufflen; i++)
-    {
-        ascii_buf[(i * 2)]     = hex_digits[(buff[i] >> 4) & 0x0F];
-        ascii_buf[(i * 2) + 1] = hex_digits[buff[i] & 0x0F];
-    }
+    uint8_t ascii_buf[2 * bufflen + 1];
+    BSL_Log_DumpAsHexString(ascii_buf, sizeof(ascii_buf), buff, bufflen);
     BSL_LOG_INFO("%s :: %s", message, ascii_buf);
-    BSL_FREE(ascii_buf);
 }
 
 int BSL_TestUtils_LoadBundleFromCBOR(BSL_TestContext_t *test_ctx, const char *cbor_seq)

test/test_BackendPolicyProvider.c
@@ -75,8 +75,7 @@ void tearDown(void)
 void test_PolicyProvider_InspectEmptyRuleset(void)
 {
     BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
-
-    strncpy(policy->name, "Unit Test Policy Provider!", sizeof(policy->name));
+    string_init_set_str(policy->name, "Unit Test Policy Provider!");
     TEST_ASSERT_EQUAL(0,
                       BSL_TestUtils_LoadBundleFromCBOR(&LocalTestCtx, RFC9173_TestVectors_AppendixA1.cbor_bundle_bib));
 
@@ -90,6 +89,7 @@ void test_PolicyProvider_InspectEmptyRuleset(void)
     TEST_ASSERT_EQUAL(0, BSL_SecurityAction_CountSecOpers(act));
 
     BSL_SecurityActionSet_Deinit(&action_set);
+    string_clear(policy->name);
 }
 
 /**
@@ -100,19 +100,16 @@ void test_PolicyProvider_InspectEmptyRuleset(void)
  */
 void test_PolicyProvider_InspectSingleBIBRuleset(void)
 {
-    {
-        BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
-        strncpy(policy->name, "Unit Test Policy Provider!", sizeof(policy->name));
+    BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
+    string_init_set_str(policy->name, "Unit Test Policy Provider!");
 
-        BSLP_PolicyPredicate_t *predicate = &policy->predicates[policy->predicate_count++];
-        BSLP_PolicyPredicate_Init(predicate, BSL_POLICYLOCATION_APPIN, BSL_TestUtils_GetEidPatternFromText("*:**"),
-                                  BSL_TestUtils_GetEidPatternFromText("*:**"),
-                                  BSL_TestUtils_GetEidPatternFromText("*:**"));
+    BSLP_PolicyPredicate_t *predicate = &policy->predicates[policy->predicate_count++];
+    BSLP_PolicyPredicate_Init(predicate, BSL_POLICYLOCATION_APPIN, BSL_TestUtils_GetEidPatternFromText("*:**"),
+                              BSL_TestUtils_GetEidPatternFromText("*:**"), BSL_TestUtils_GetEidPatternFromText("*:**"));
 
-        BSLP_PolicyRule_t *rule = &policy->rules[policy->rule_count++];
-        BSLP_PolicyRule_Init(rule, "Verify BIB on APPIN from anywhere", predicate, 1, BSL_SECROLE_VERIFIER,
-                             BSL_SECBLOCKTYPE_BIB, BSL_BLOCK_TYPE_PAYLOAD, BSL_POLICYACTION_DROP_BUNDLE);
-    }
+    BSLP_PolicyRule_t *rule = &policy->rules[policy->rule_count++];
+    BSLP_PolicyRule_Init(rule, "Verify BIB on APPIN from anywhere", predicate, 1, BSL_SECROLE_VERIFIER,
+                         BSL_SECBLOCKTYPE_BIB, BSL_BLOCK_TYPE_PAYLOAD, BSL_POLICYACTION_DROP_BUNDLE);
 
     TEST_ASSERT_EQUAL(0,
                       BSL_TestUtils_LoadBundleFromCBOR(&LocalTestCtx, RFC9173_TestVectors_AppendixA1.cbor_bundle_bib));
@@ -125,6 +122,7 @@ void test_PolicyProvider_InspectSingleBIBRuleset(void)
     TEST_ASSERT_EQUAL(1, BSL_SecurityAction_CountSecOpers(BSL_SecurityActionSet_GetActionAtIndex(&action_set, 0)));
 
     BSL_SecurityActionSet_Deinit(&action_set);
+    string_clear(policy->name);
 }
 
 /**
@@ -133,7 +131,7 @@ void test_PolicyProvider_InspectSingleBIBRuleset(void)
 void test_PolicyProvider_Inspect_RFC9173_BIB(void)
 {
     BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
-    strncpy(policy->name, "Unit Test Policy Provider!", sizeof(policy->name));
+    string_init_set_str(policy->name, "Unit Test Policy Provider!");
 
     BSLP_PolicyPredicate_t *predicate = &policy->predicates[policy->predicate_count++];
     BSLP_PolicyPredicate_Init(predicate, BSL_POLICYLOCATION_APPIN, BSL_TestUtils_GetEidPatternFromText("*:**"),
@@ -160,6 +158,7 @@ void test_PolicyProvider_Inspect_RFC9173_BIB(void)
     TEST_ASSERT_EQUAL(3, BSL_SecOper_CountParams(BSL_SecurityAction_GetSecOperAtIndex(act, 0)));
 
     BSL_SecurityActionSet_Deinit(&action_set);
+    string_clear(policy->name);
 }
 
 // TODO - test with also setting sec pararms and other things and test the RFC 9173 things.
@@ -179,11 +178,11 @@ void test_MultiplePolicyProviders(void)
 
     BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
     policy->pp_id                 = BSL_SAMPLE_PP_ID;
-    strncpy(policy->name, "Unit Test Policy Provider 1!", sizeof(policy->name));
+    string_init_set_str(policy->name, "Unit Test Policy Provider 1!");
 
     BSLP_PolicyProvider_t *policy2 = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID_2)->user_data;
     policy2->pp_id                 = BSL_SAMPLE_PP_ID_2;
-    strncpy(policy2->name, "Unit Test Policy Provider 2!", sizeof(policy2->name));
+    string_init_set_str(policy2->name, "Unit Test Policy Provider 2!");
 
     BSLP_PolicyPredicate_t *predicate = &policy->predicates[policy->predicate_count++];
     BSLP_PolicyPredicate_Init(predicate, BSL_POLICYLOCATION_APPIN, BSL_TestUtils_GetEidPatternFromText("*:**"),
@@ -246,4 +245,6 @@ void test_MultiplePolicyProviders(void)
 
     BSL_SecurityActionSet_Deinit(&action_set);
     BSL_FREE(response_set);
+    string_clear(policy->name);
+    string_clear(policy2->name);
 }

test/test_PublicInterfaceImpl.c
@@ -65,7 +65,7 @@ void setUp(void)
     TEST_ASSERT_EQUAL(0, BSL_API_RegisterPolicyProvider(&LocalTestCtx.bsl, BSL_SAMPLE_PP_ID, policy_desc));
 
     BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
-    strncpy(policy->name, "Unit Test Policy Provider!", sizeof(policy->name));
+    string_init_set_str(policy->name, "Unit Test Policy Provider!");
 
     BSL_SecParam_t param_scope_flag = { 0 };
     BSL_SecParam_InitInt64(&param_scope_flag, RFC9173_BIB_PARAMID_INTEG_SCOPE_FLAG, 0);
@@ -481,6 +481,8 @@ void setUp(void)
 void tearDown(void)
 {
     BSL_SecurityActionSet_Deinit(&action_set);
+    BSLP_PolicyProvider_t *policy = BSL_PolicyDict_get(LocalTestCtx.bsl.policy_reg, BSL_SAMPLE_PP_ID)->user_data;
+    string_clear(policy->name);
     mock_bpa_ctr_deinit(&LocalTestCtx.mock_bpa_ctr);
     BSL_CryptoDeinit();
     TEST_ASSERT_EQUAL(0, BSL_API_DeinitLib(&LocalTestCtx.bsl));

commit b7be485f8404019ad266c949539646e71d9ff163

Fix logging errors introduced in #86

src/policy_provider/SamplePolicyProvider.c
@@ -120,12 +120,12 @@ int BSLP_QueryPolicy(const void *user_data, BSL_SecurityActionSet_t *output_acti
     {
         const BSLP_PolicyRule_t *rule = &self->rules[index];
         CHK_PROPERTY(BSLP_PolicyRule_IsConsistent(rule));
-        BSL_LOG_DEBUG("Evaluating against rule `%s`", rule->description);
+        BSL_LOG_DEBUG("Evaluating against rule `%s`", m_string_get_cstr(rule->description));
 
         if (!BSLP_PolicyPredicate_IsMatch(rule->predicate, location, primary_block.field_src_node_id,
                                           primary_block.field_dest_eid))
         {
-            BSL_LOG_DEBUG("Rule `%s` not a match", rule->description);
+            BSL_LOG_DEBUG("Rule `%s` not a match", m_string_get_cstr(rule->description));
             continue;
         }
 
@@ -209,7 +209,7 @@ int BSLP_QueryPolicy(const void *user_data, BSL_SecurityActionSet_t *output_acti
                 BSLP_SecOperPtrList_push_back(secops, sec_oper);
             }
         }
-        BSL_LOG_INFO("Created sec operation for rule `%s`", rule->description);
+        BSL_LOG_INFO("Created sec operation for rule `%s`", m_string_get_cstr(rule->description));
     }
     BSL_PrimaryBlock_deinit(&primary_block);
 
@@ -370,7 +370,7 @@ int BSLP_PolicyRule_Init(BSLP_PolicyRule_t *self, const char *desc, BSLP_PolicyP
 void BSLP_PolicyRule_Deinit(BSLP_PolicyRule_t *self)
 {
     ASSERT_ARG_EXPR(BSLP_PolicyRule_IsConsistent(self));
-    BSL_LOG_INFO("BSLP_PolicyRule_Deinit: %s, nparams=%zu", self->description, self->nparams);
+    BSL_LOG_INFO("BSLP_PolicyRule_Deinit: %s, nparams=%zu", m_string_get_cstr(self->description), self->nparams);
     string_clear(self->description);
     BSL_FREE(self->params);
     memset(self, 0, sizeof(*self));
@@ -427,7 +427,7 @@ int BSLP_PolicyRule_EvaluateAsSecOper(const BSLP_PolicyRule_t *self, BSL_SecOper
         uint8_t *ptr    = &((uint8_t *)(self->params))[offset];
         BSL_SecOper_AppendParam(sec_oper, (BSL_SecParam_t *)ptr);
     }
-    BSL_LOG_INFO("Created sec operation for rule `%s`", self->description);
+    BSL_LOG_INFO("Created sec operation for rule `%s`", m_string_get_cstr(self->description));
 
     return BSL_SUCCESS;
 }