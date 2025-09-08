85953b2e765d64e91dbf1c8278a1794b97fac89c
Trey Perry <trey.perry@jhuapl.edu>
2025-09-08 11:43:37 -0400

    Fixed unit test error

--- src/mock_bpa/agent.c
+++ src/mock_bpa/agent.c
@@ -469,39 +469,39 @@ int MockBPA_Agent_Init(MockBPA_Agent_t *agent)
     // TODO find a better way to deal with this
     {
         agent->appin.policy               = BSL_CALLOC(1, sizeof(BSLP_PolicyProvider_t));
+        agent->appin.policy->pp_id = 1;
         BSL_PolicyDesc_t policy_callbacks = (BSL_PolicyDesc_t) { .deinit_fn   = BSLP_Deinit,
                                                                  .query_fn    = BSLP_QueryPolicy,
                                                                  .finalize_fn = BSLP_FinalizePolicy,
                                                                  .user_data   = agent->appin.policy };
         ASSERT_PROPERTY(BSL_SUCCESS == BSL_API_RegisterPolicyProvider(agent->appin.bsl, 1, policy_callbacks));
-        agent->appin.policy->pp_id = 1;
     }
     {
         agent->appout.policy              = BSL_CALLOC(1, sizeof(BSLP_PolicyProvider_t));
+        agent->appout.policy->pp_id = 1;
         BSL_PolicyDesc_t policy_callbacks = (BSL_PolicyDesc_t) { .deinit_fn   = BSLP_Deinit,
                                                                  .query_fn    = BSLP_QueryPolicy,
                                                                  .finalize_fn = BSLP_FinalizePolicy,
                                                                  .user_data   = agent->appout.policy };
         ASSERT_PROPERTY(BSL_SUCCESS == BSL_API_RegisterPolicyProvider(agent->appout.bsl, 1, policy_callbacks));
-        agent->appout.policy->pp_id = 1;
     }
     {
         agent->clin.policy                = BSL_CALLOC(1, sizeof(BSLP_PolicyProvider_t));
+        agent->clin.policy->pp_id = 1;
         BSL_PolicyDesc_t policy_callbacks = (BSL_PolicyDesc_t) { .deinit_fn   = BSLP_Deinit,
                                                                  .query_fn    = BSLP_QueryPolicy,
                                                                  .finalize_fn = BSLP_FinalizePolicy,
                                                                  .user_data   = agent->clin.policy };
         ASSERT_PROPERTY(BSL_SUCCESS == BSL_API_RegisterPolicyProvider(agent->clin.bsl, 1, policy_callbacks));
-        agent->clin.policy->pp_id = 1;
     }
     {
         agent->clout.policy               = BSL_CALLOC(1, sizeof(BSLP_PolicyProvider_t));
+        agent->clout.policy->pp_id = 1;
         BSL_PolicyDesc_t policy_callbacks = (BSL_PolicyDesc_t) { .deinit_fn   = BSLP_Deinit,
                                                                  .query_fn    = BSLP_QueryPolicy,
                                                                  .finalize_fn = BSLP_FinalizePolicy,
                                                                  .user_data   = agent->clout.policy };
         ASSERT_PROPERTY(BSL_SUCCESS == BSL_API_RegisterPolicyProvider(agent->clout.bsl, 1, policy_callbacks));
-        agent->clout.policy->pp_id = 1;
     }
 
     agent->over_addr.sin_family   = 0;
