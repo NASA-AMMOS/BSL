e4d27b9de15236b3b5f5ee91bc1c17be2b631918
Joshua Stone <joshua.stone@jhuapl.edu>
2026-03-14 15:11:59 -0400

    Add docs to host desc struct for dynamic mem cbs

--- src/BPSecLib_Public.h
+++ src/BPSecLib_Public.h
@@ -351,16 +351,30 @@ typedef struct
     /// @brief Host BPA function that returns true if the given EID matched an EID pattern.
     bool (*eidpat_match)(const BSL_HostEIDPattern_t *pat, const BSL_HostEID_t *eid, void *user_data);
 
-    /// @brief Dynamic memory allocation callback. Returns valid heap pointer on success, NULL on failure.
+    /** (Optionally set) Dynamic memory allocation callback. 
+     *  Defaults to libc malloc if unset.
+     * 
+     *  @return valid heap pointer on success, NULL on failure.
+     */
     void *(*malloc_cb)(size_t size);
 
-    /// @brief Dynamic memory re-allocation callback. Returns valid heap pointer on success, NULL on failure.
+    /** (Optionally set) Dynamic memory re-allocation callback. 
+     *  Defaults to libc realloc if unset.
+     * 
+     *  @return valid heap pointer on success, NULL on failure.
+     */
     void *(*realloc_cb)(void *ptr, size_t size);
 
-    /// @brief Contiguous dynamic memory allocation callback. Returns valid 0-initialized heap pointer on success, NULL on failure.
+    /** (Optionally set) Contiguous dynamic memory allocation callback.
+     *  Defaults to libc calloc if unset
+     * 
+     *  @return valid 0-initialized heap pointer on success, NULL on failure.
+    */
     void *(*calloc_cb)(size_t nmemb, size_t size);
 
-    /// @brief Free dynamic memory allocation callback.
+    /** (Optionally set) Free dynamic memory allocation callback. 
+     *  Defaults to libc free if unset.
+     */
     void (*free_cb)(void* ptr);
 
 } BSL_HostDescriptors_t;
