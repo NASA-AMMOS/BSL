7baf51975210482ca8fc2beb0c5033dfb69269c4
Brian Sipos <brian.sipos@jhuapl.edu>
2026-04-24 10:08:27 -0400

    fix spelling

--- src/policy_provider/SamplePolicyConfigParser.h
+++ src/policy_provider/SamplePolicyConfigParser.h
@@ -96,8 +96,8 @@ int BSLP_RegisterPolicyFromJSON(const char *policy_cfg_path, BSLP_PolicyProvider
 typedef uint32_t BSLP_BitstringPolicyConfiguration_t;
 
 /**
- * Initialize local policy provider from list of bitstrings
- * @param[in] policies comma seperated policy bitstrings as described by @ref BSLP_BitstringPolicyConfiguration_t
+ * Initialize local policy provider from list of bit strings
+ * @param[in] policies comma seperated policy bit strings as described by @ref BSLP_BitstringPolicyConfiguration_t
  * @param[in,out] policy policy provider to configure. Must be initialize/allocated
  */
 int BSLP_RegisterPolicyFromBitstringList(const char *policies, BSLP_PolicyProvider_t *policy);
