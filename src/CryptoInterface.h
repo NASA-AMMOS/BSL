263b55a12e598cfc9a148078b07e2cbf959d1af4
Brian Sipos <brian.sipos@jhuapl.edu>
2026-07-07 20:32:47 -0400

    Missed in last commit

--- src/CryptoInterface.h
+++ src/CryptoInterface.h
@@ -366,7 +366,7 @@ int BSL_Crypto_GenKey(size_t key_length, BSL_Crypto_KeyHandle_t *key_out);
 /**
  * Generate initialization vector (IV) for AES-GCM for BCBs
  * @param[in,out] buf to write data into without changing its size.
- * The size in bytes of iv (MUST be between 8-16, SHOULD be 12 @cite rfc9173)
+ * The size in bytes of iv is determined by the cipher algorithm.
  * @returns 0 if successful
  */
 int BSL_Crypto_GenIV(BSL_Data_t *buf);
