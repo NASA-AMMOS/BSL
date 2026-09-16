281ef33bd05d89b7feabe83be70fc9ddc0bbf454
Brian Sipos <brian.sipos@jhuapl.edu>
2026-09-16 13:12:35 -0400

    Remove unused counter and associated API

--- src/bsl/BPSecLib_Private.h
+++ src/bsl/BPSecLib_Private.h
@@ -1092,25 +1092,49 @@ int BSL_PolicyRegistry_InspectActions(const BSL_LibCtx_t *bsl, BSL_SecurityActio
 int BSL_PolicyRegistry_FinalizeActions(const BSL_LibCtx_t *bsl, const BSL_SecurityActionSet_t *policy_actions,
                                        BSL_BundleRef_t *bundle);
 
-/// @brief Callback interface to query policy provider to populate the action set
+/** @brief Callback interface to query policy provider to populate the action set.
+ *
+ * @param[in] user_data The pointer registered by BSL_PolicyDesc_s::user_data.
+ * @param[in,out] output_action_set The action set already initialized to store the query result.
+ * @param[in] bundle The bundle to inspect.
+ * @param location The interaction point of this query.
+ * @return BSL_SUCCESS if successful.
+ */
 typedef int (*BSL_PolicyInspect_f)(void *user_data, BSL_SecurityActionSet_t *output_action_set,
                                    const BSL_BundleRef_t *bundle, BSL_PolicyLocation_e location);
 
-/// @brief Callback interface to finalize policy provider over the action set. Finalize should ignore actions from
-/// different policy providers
+/** @brief Callback interface to finalize policy provider over the action set.
+ * Finalize should ignore actions from different policy providers.
+ *
+ * @param[in] user_data The pointer registered by BSL_PolicyDesc_s::user_data.
+ * @param[in,out] output_action_set The action set which was executed with outcomes in each
+ * contained operation.
+ * @return BSL_SUCCESS if successful.
+ */
 typedef int (*BSL_PolicyFinalize_f)(void *user_data, const BSL_SecurityActionSet_t *output_action_set,
                                     BSL_BundleRef_t *bundle);
 
-/// @brief Callback interface for policy provider to shut down and release any resources
+/** @brief Callback interface for policy provider to shut down and release any resources
+ *
+ * @param[in] user_data The pointer registered by BSL_PolicyDesc_s::user_data.
+ */
 typedef void (*BSL_PolicyDeinit_f)(void *user_data);
 
 /// @brief Descriptor of opaque data and callbacks for Policy Provider.
 struct BSL_PolicyDesc_s
 {
-    void                *user_data;   ///< Reference to policy provider -specific data
-    BSL_PolicyInspect_f  query_fn;    ///< Function pointer to query policy
-    BSL_PolicyFinalize_f finalize_fn; ///< Function pointer to finalize policy
-    BSL_PolicyDeinit_f   deinit_fn;   ///< Function to deinit the policy provider at termination of BSL context
+    /** Reference to policy provider-specific context data.
+     * This pointer may be NULL.
+     */
+    void *user_data;
+    /// Function pointer to query policy and get actions/operations
+    BSL_PolicyInspect_f query_fn;
+    /// Function pointer to finalize policy after actions/operations
+    BSL_PolicyFinalize_f finalize_fn;
+    /** Function to deinit the policy provider at termination of BSL context.
+     * This pointer may be NULL.
+     */
+    BSL_PolicyDeinit_f deinit_fn;
 };
 
 /** Call the underlying security context to perform the given action set
